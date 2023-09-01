4f5cf07b14363a174698e19c6de1059dd4e85a4a
James-Lambertcoding <124365913+James-Lambertcoding@users.noreply.github.com>
2023-09-01 20:04:28 +0100

    Finish for Friday
    Includes 2.5 Draft

--- R Model - Regressions/R Code Diss/Fixed_Effects_Regression_v1.R
+++ R Model - Regressions/R Code Diss/Fixed_Effects_Regression_v1.R
@@ -15,13 +15,13 @@ library(lfe)
 ## 1.0 Data ------------------
 
 data_name_1 <- "Price_data/regression_data_hh.csv"
-data_name_2 <- "Price_data/Simple_All_data.csv"
-data_name_3 <- "Price_data/multiple_regression_data.csv"
+data_name_2 <- "Price_data/price_quantity.csv"
+#data_name_3 <- "Price_data/multiple_regression_data.csv"
 
 hh_df <- read.csv(file = data_name_1, stringsAsFactors = F)
 sh_df <- read.csv(file = data_name_2, stringsAsFactors = F)
-mh_df <- read.csv(file = data_name_3, stringsAsFactors = F)
-
+#mh_df <- read.csv(file = data_name_3, stringsAsFactors = F)
+mh_df <- sh_df
 
 hh_df_1 <- hh_df %>% 
   filter(Incl ==1)
@@ -48,7 +48,7 @@ good_types_name <- c("Food & non-alcoholic drinks",
 good_one_trans <- good_types_name[-7]
 
 
-## 2.0 Income Elasticity -------------
+## 2.0 Data Manipulation -------------
 
 good_types <- unique(sh_df[,"Good.Name"])
 
@@ -60,7 +60,7 @@ for(i in 1:length(good_types)){
   
   split_list[[i]] <- sh_df %>% 
     filter(Good == i) %>% 
-    filter(Spend !=0) %>% 
+    filter(Quantity !=0) %>% 
     mutate(log_income = log(Income.Amount)) %>% 
     mutate(log_price = log(Price)) %>% 
     mutate(Income.Bracket = paste0("perc_",Income.Group*10)) %>% 
@@ -72,11 +72,39 @@ for(i in 1:length(good_types)){
   
   multi_list[[i]] <- mh_df %>% 
     filter(Good == i) %>% 
-    filter(Spend !=0) %>% 
+    filter(Quantity !=0) %>% 
     select(-Price_7)
   
   
 }
 
+## 3.0 Fixed Effects -----------
 
-for
+
+fe_list <- list()
+fe_df <- data.frame( "good" = good_types,
+                     "coef_1" = rep(0),
+                     "coef_1_pr" = rep(0),
+                     "model_r2" = rep(0),
+                     "model_ad_r2" = rep(0))
+
+
+for(i in 1:length(good_types)){
+  
+  ## Run Regression
+  fe_list[[i]] <- felm(Quantity ~ Price|Income.Group,data = split_list[[i]])  
+  
+  ## Input Summary into data frame
+  fe_df[i,2] <- summary(fe_list[[i]])[[7]][1]
+  fe_df[i,3] <- summary(fe_list[[i]])[[7]][4]
+  fe_df[i,4] <- summary(fe_list[[i]])[22]
+  fe_df[i,5] <- summary(fe_list[[i]])[[23]]
+  
+}
+
+## 4.0 Save Results
+
+fe_df <- fe_df %>% 
+  filter(good != 7)
+
+write.csv(x = fe_df,file = "Output_3/Linear Coefs/Price_Fixed_Effects.csv")
